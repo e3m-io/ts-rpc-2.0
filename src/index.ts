22fdc54f70919fdf6d450dcebc4df91c0bf4b59a
e3m-io <66180948+e3m-io@users.noreply.github.com>
2025-07-05 11:39:12 -0700

    fix: export `RpcRequestId` and `RpcRequestParams`

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export * from "./parseRpcMessage.ts";
 export * from "./RpcMessage.ts";
 export * from "./RpcNotification.ts";
 export * from "./RpcRequest.ts";
+export * from "./RpcRequestId.ts";
+export * from "./RpcRequestParams.ts";
 export * from "./RpcResponse.ts";
 export * from "./RpcResponseError.ts";
 export * from "./RpcResponseResult.ts";
